9fa1db9699bfbdcb4bc0eda291467d9f224da9ae
Hi9 <1914619958@qq.com>
2023-05-18 22:44:56 +0800

    b2 finished

---
+++ lesson2/apps/b2/src/main.rs
@@ -0,0 +1,50 @@
+#![no_std]
+#![no_main]
+
+use drv0 as _;
+use drv1 as _;
+
+use drv_common::CallEntry;
+
+#[no_mangle]
+fn main() {
+    libos::init();
+
+    libos::println!("\n[ArceOS Tutorial]: B2\n");
+    verify();
+}
+
+// 在libos/linker_riscv64.lds中定义，用于指示 .init_calls 段的开始和结束地址
+extern "C" {
+    fn __init_calls_start();
+    fn __init_calls_end();
+}
+
+fn traverse_drivers() {
+    // Parse range of init_calls by calling C function.
+    let range_start = __init_calls_start as usize;
+    let range_end = __init_calls_end as usize;
+    display_initcalls_range(range_start, range_end);
+
+    let size = core::mem::size_of::<&CallEntry>();
+    for addr in (range_start..range_end).step_by(size) {
+        let entry = unsafe { core::mem::transmute::<usize, &CallEntry>(addr) };
+        let drv = (entry.init_fn)();
+        // For each driver, display name & compatible
+        display_drv_info(drv.name, drv.compatible);
+    }
+}
+
+fn display_initcalls_range(start: usize, end: usize) {
+    libos::println!("init calls range: 0x{:X} ~ 0x{:X}\n", start, end);
+}
+
+fn display_drv_info(name: &str, compatible: &str) {
+    libos::println!("Found driver '{}': compatible '{}'", name, compatible);
+}
+
+fn verify() {
+    traverse_drivers();
+
+    libos::println!("\nResult: Okay!");
+}
--- lesson2/drv_common/src/lib.rs
+++ lesson2/drv_common/src/lib.rs
@@ -19,3 +19,18 @@ type InitFn = fn() -> Driver<'static>;
 pub struct CallEntry {
     pub init_fn: InitFn,
 }
+
+#[macro_export]
+macro_rules! module {
+    (type: $module_type:ident, name: $module_name:expr, compatible: $module_compatible:expr) => {
+        #[used]
+        #[link_section = ".init_calls"]
+        static DRV0_ENTRY: CallEntry = CallEntry {
+            init_fn: drv0_init_fn
+        };
+
+        fn drv0_init_fn() -> $module_type<'static> {
+            $module_type::info($module_name, $module_compatible)
+        }
+    };
+}
